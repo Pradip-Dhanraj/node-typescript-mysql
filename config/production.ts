a33dff0503c35a2d17f23a9117ea9f97a7651f5a
Pradip-Dhanraj <pradipdhanraj@gmail.com>
2022-04-15 02:33:13 +0530

    minor fixes

--- config/production.ts
+++ config/production.ts
@@ -1,9 +1,9 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
-// export default {
-//     port : process.env.PORT || 3000,
-//     username :process.env.USERNAME || "",
-//     pssword :process.env.PASSWORD || "",
-//     dbUrl :process.env.DB_CONNECTION || "",
-// }
+export default {
+    port: process.env.PORT,
+    username: process.env.USERNAME,
+    pssword: process.env.PASSWORD,
+    dbUrl: process.env.DB_CONNECTION,
+}
