223476bd5f98ce8ff949b190d22e94a57abff4b5
Pradip-Dhanraj <pradipdhanraj@gmail.com>
2022-04-15 03:00:31 +0530

    minor fixes

--- config/production.ts
+++ config/production.ts
@@ -3,7 +3,9 @@ dotenv.config();
 
 export default {
     port: process.env.PORT,
-    username: process.env.USERNAME,
-    pssword: process.env.PASSWORD,
-    dbUrl: process.env.DB_CONNECTION,
+    username: process.env.DB_USERNAME,
+    pssword: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT,
 }
